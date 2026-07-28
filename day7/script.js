fefac477583b768fc3bb58ed6d4364e520b2605f
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-07-28 16:28:56 +0530

    if else if ? in javascript

---
+++ day7/script.js
@@ -0,0 +1,24 @@
+// console.log("hello world hoohoooohooooohc")
+
+let a = 10;
+
+
+let c = a > 18 ? "you are eligible to vote" : "you are not eligible to vote"
+
+console.log(c)
+
+if(a > 18){
+    console.log("you are eligible to vote")
+}
+else if(a < 0 || a > 100){
+    console.log("invalid age")
+}
+
+else if(a < 18){
+    console.log("you are not eligible to vote")
+}
+
+
+else{
+    console.log("you are not eligible to vote")
+}

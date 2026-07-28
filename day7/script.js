1779c2a63b2448f4b0b503115d9f197b852e4ff9
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-07-28 17:39:42 +0530

    made a folty calculater

--- day7/script.js
+++ day7/script.js
@@ -1,15 +1,48 @@
-function something(name){
-    // console.log("Hello " + name);
-    // console.log("How are you, " + name + "?");
+let rand = Math.random() * 10;
 
+// console.log(rand);
+
+function rightadd(a, b){
+    return a + b;
 }
-
-something("John");
-something("Alice");
-
+function rightsub(a, b){
+    return a - b;
+}
+function rightmult(a, b){
+    return a * b;
+}
+function rightdiv(a, b){
+    return a / b;
+}
+function div(a, b){
+    return a * b;
+}
 function add(a, b){
+    return a * b;
+}
+function sub(a, b){
+    return a / b;
+}
+function mult(a, b){
     return a + b;
 }
 
-let sum = add(5, 10);
-console.log("The sum is: " + sum);
+let x = 5;
+let y = 10;
+
+if (rand > 2){
+    console.log("right this time ");
+    console.log("Addition: " + rightadd(x, y));
+    console.log("Subtraction: " + rightsub(x, y));
+    console.log("Multiplication: " + rightmult(x, y));
+    console.log("Division: " + rightdiv(x, y));
+}
+else {
+    console.log("wrong this time ");
+    console.log("Addition: " + add(x, y));
+    console.log("Subtraction: " + sub(x, y));
+    console.log("Multiplication: " + mult(x, y));
+    console.log("Division: " + div(x, y));
+}
+
+

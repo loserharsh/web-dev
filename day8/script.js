df6832477ef7a564b9ed98c9dcbceab5c888bba0
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-07-31 00:31:08 +0530

    DOM child

--- day8/script.js
+++ day8/script.js
@@ -4,5 +4,13 @@ document.body.style.backgroundColor = "lightblue";
 const heading = document.createElement("h1");
 heading.textContent = "Welcome to DOM Manipulation!";
 document.body.appendChild(heading);
-console.log("document.title:", document.title);
-console.log(document.body);
+// console.log("document.title:", document.title);
+// console.log(document.body);
+
+document.body.firstElementChild.style.backgroundColor = "lightgreen";
+
+document.body.firstElementChild.style.fontSize = "2em";
+const thischild = document.body.firstElementChild.firstElementChild;
+thischild.style.color = "red";
+thischild.style.fontSize = "1.5em";
+thischild.style.backgroundColor = "teal";

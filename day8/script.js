76b74b0d06cc09988442d246223d42490273ef1d
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-07-31 00:53:50 +0530

    query selector

--- day8/script.js
+++ day8/script.js
@@ -14,3 +14,29 @@ const thischild = document.body.firstElementChild.firstElementChild;
 thischild.style.color = "red";
 thischild.style.fontSize = "1.5em";
 thischild.style.backgroundColor = "teal";
+
+
+const newdiv = document.getElementsByClassName("bax")[2];
+
+newdiv.style.backgroundColor = "yellow";
+newdiv.style.border = "2px solid black";
+
+const bx5 = document.getElementById("box5");
+bx5.style.backgroundColor = "orange";
+bx5.style.border = "2px solid black";
+
+
+const dis = document.querySelector(".bax");
+dis.style.display = "flex";
+dis.style.justifyContent = "center";
+
+const allboxes = document.querySelectorAll(".bax");
+// allboxes.forEach((element) => {
+//     element.style.display = "flex";
+//     element.style.justifyContent = "center";
+// });
+
+for (let i = 0; i < allboxes.length; i++) {
+    allboxes[i].style.display = "flex";
+    allboxes[i].style.justifyContent = "center";
+}

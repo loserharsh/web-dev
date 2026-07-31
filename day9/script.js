2f68ece43e16b4e482267656902f884b26e1882d
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-07-31 14:19:55 +0530

    At random color changing boxs form javascript

--- day9/script.js
+++ day9/script.js
@@ -1,7 +1,29 @@
-let diav = document.createElement("div");
-diav.setAttribute('id', 'myDiv');
-document.querySelector(".container").append(diav); 
-diav.innerHTML = "Hello, World!";
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray",
+    "cyan", "magenta", "lime", "teal", "indigo", "violet", "gold", "silver", "maroon", "navy", "olive"];
 
-let cont = document.querySelector(".container");
-cont.insertAdjacentHTML('afterend', '<p>This is a new paragraph added to the container.</p>');
+
+const boxing = document.querySelectorAll(".box");
+boxing.forEach((box) => {
+    box.style.display = "flex";
+    box.style.justifyContent = "center";
+    box.style.alignItems = "center";
+    box.style.width = "100px";
+    box.style.height = "100px";
+    box.style.border = "2px solid black";
+    
+});
+
+const container = document.querySelector(".container");
+container.style.display = "flex";
+container.style.flex = "0 0 100%";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
+container.style.gap = "20px";
+container.style.height = "100vh";
+container.style.width = "100%";
+
+
+boxing.forEach((box) => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    box.style.backgroundColor = randomColor;
+});
---
+++ day9/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray", "black"];
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+console.log(randomColor);

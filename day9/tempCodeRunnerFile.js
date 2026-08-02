455cf321348d252cb28973ab55bf1853fe92eba6
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-02 23:49:48 +0530

    callback

--- day9/tempCodeRunnerFile.js
+++ day9/tempCodeRunnerFile.js
@@ -1,3 +1,2 @@
-const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray", "black"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+
+// const titles = document.querySelector(".title");

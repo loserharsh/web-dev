1ceb10e319c4b04df2ab3f9839ff11c6d232ea45
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-03 23:33:24 +0530

    something

--- day9/newscript.js
+++ day9/newscript.js
@@ -1,21 +1,21 @@
-// let a = document.querySelector(".btn");
-// const titles = document.querySelector(".title");
+let a = document.querySelector(".btn");
+const titles = document.querySelector(".title");
 
-// a.addEventListener("click", () => {
-//     titles.style.backgroundColor = "red";
-//     titles.innerHTML = "You clicked the button!";
+a.addEventListener("click", () => {
+    titles.style.backgroundColor = "red";
+    titles.innerHTML = "You clicked the button!";
 
-// });
-// a.addEventListener("dblclick", () => {
-//     titles.style.backgroundColor = "red";
-//     titles.innerHTML = "You double-clicked the button!";
-// });
-// a.addEventListener("mouseenter", () => {
-//     titles.style.backgroundColor = "red";
-// });
-// a.addEventListener("mouseleave", () => {
-//     titles.style.backgroundColor = "white";
-// });
+});
+a.addEventListener("dblclick", () => {
+    titles.style.backgroundColor = "red";
+    titles.innerHTML = "You double-clicked the button!";
+});
+a.addEventListener("mouseenter", () => {
+    titles.style.backgroundColor = "red";
+});
+a.addEventListener("mouseleave", () => {
+    titles.style.backgroundColor = "white";
+});
 const callback = (name) => {
     console.log("hello " + name);
 }

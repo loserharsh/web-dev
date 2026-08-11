707c297674fe33418c2405a3647c1030f06cc16c
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-11 21:05:08 +0530

    more floting windows wow

--- project/databaseditative/script.js
+++ project/databaseditative/script.js
@@ -4,9 +4,9 @@ let startX = 0;
 let startY = 0;
 
 const flot = document.querySelector('.flot');
-
+const flot1 = document.querySelector('.flot1');
 flot.addEventListener('mousedown', mousedown);
-
+flot1.addEventListener('mousedown', mousedown);
 function mousedown(e) {
     startX = e.clientX;
     startY = e.clientY;
@@ -18,10 +18,8 @@ function mousedown(e) {
 function mousemove(e) {
     newX =  startX - e.clientX ;
     newY = startY - e.clientY;
-
     startX = e.clientX;
     startY = e.clientY;
-
     flot.style.top = (flot.offsetTop - newY)  + 'px';
     flot.style.left = (flot.offsetLeft - newX) + 'px';
 

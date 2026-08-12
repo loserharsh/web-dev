fdacffe1fe957789ed4b2a7f90f857bb0840ac79
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-12 22:14:44 +0530

    done spwoning window with js and closing  too

--- project/databaseditative/atm.js
+++ project/databaseditative/atm.js
@@ -1,24 +1,19 @@
+const template = document.getElementById('box-template');
 
-class creatingb{
-    constructor(height, width, color, classname, top, left){
-        this.height = height;
-        this.width = width;
-        this.color = color; 
-        this.classname = classname;
-        this.top = top;
-        this.left = left;
-        const mainDiv = document.createElement('div');
-        mainDiv.setAttribute('class', classname);
-        document.querySelector('body').appendChild(mainDiv);
-        mainDiv.style.position = 'absolute';
-        mainDiv.style.top = this.top + 'px';
-        mainDiv.style.left = this.left + 'px';
-        mainDiv.style.backgroundColor = this.color;
-        mainDiv.style.height = this.height + 'px';
-        mainDiv.style.width = this.width + 'px';
-    }
+function createBox() {
+    const box = template.content.cloneNode(true);
+
+    const closeButton = box.querySelector('.close');
+
+    closeButton.addEventListener('click', () => {
+        closeButton.closest('.box').remove();
+    });
+
+    document.body.appendChild(box);
 }
 
-const create = new creatingb(100, 100, '#5e8583', 'flot', 10, 10);
-const create2 = new creatingb(100, 100, '#64bac2', 'flot1', 10, 120);
-const create3 = new creatingb(100, 100, '#64bac2', 'flot2', 10, 230);
+const spawn = document.querySelector('.app');
+
+spawn.addEventListener('click', () => {
+    createBox();
+});

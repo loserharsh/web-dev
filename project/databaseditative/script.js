f46d9b5ab520285264bc6769166d97719f70f4bf
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-12 16:36:15 +0530

    using class

--- project/databaseditative/script.js
+++ project/databaseditative/script.js
@@ -4,28 +4,35 @@ let startX = 0;
 let startY = 0;
 
 const flot = document.querySelector('.flot');
-const flot1 = document.querySelector('.flot1');
-flot.addEventListener('mousedown', mousedown);
-flot1.addEventListener('mousedown', mousedown);
-function mousedown(e) {
-    startX = e.clientX;
-    startY = e.clientY;
+class Flot {
+    constructor(name) {
+        this.name = name;
+        this.flot = document.querySelector(`.${name}`);
+        this.flot.addEventListener('mousedown', this.mousedown.bind(this));
+    }
 
-    document.addEventListener('mousemove', mousemove);
-    document.addEventListener('mouseup', mouseup);
-};
+    mousedown(e) {
+        startX = e.clientX;
+        startY = e.clientY;
 
-function mousemove(e) {
-    newX =  startX - e.clientX ;
-    newY = startY - e.clientY;
-    startX = e.clientX;
-    startY = e.clientY;
-    flot.style.top = (flot.offsetTop - newY)  + 'px';
-    flot.style.left = (flot.offsetLeft - newX) + 'px';
+        document.addEventListener('mousemove', this.mousemove.bind(this));
+        document.addEventListener('mouseup', this.mouseup.bind(this));
+    }
 
+    mousemove(e) {
+        newX = startX - e.clientX;
+        newY = startY - e.clientY;
+        startX = e.clientX;
+        startY = e.clientY;
+        this.flot.style.top = (this.flot.offsetTop - newY) + 'px';
+        this.flot.style.left = (this.flot.offsetLeft - newX) + 'px';
 
-};
+    }
 
-function mouseup(e){
-    document.removeEventListener('mousemove', mousemove);
-};
+    mouseup(e) {
+        document.removeEventListener('mouseup', this.mouseup.bind(this));
+    }
+
+}
+
+const flotInstance = new Flot('flot');

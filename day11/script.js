1ef22bd69a70e5aa1109240f77917947dedc0413
harshwardhan kailasia <harshwardhan1617@gmail.com>
2026-08-18 21:13:43 +0530

    starting API

---
+++ day11/script.js
@@ -0,0 +1,12 @@
+let stunder = {
+    name: "Stunder",
+    age: 25
+}
+
+// 
+// https://catfact.ninja/fact
+
+// {
+//   "fact": "Among many other diseases, cats can suffer from anorexia, senility, feline AIDS and acne.",
+//   "length": 89
+// }
